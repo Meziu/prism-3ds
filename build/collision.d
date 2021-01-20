14abb2dc9787b100e80df2765525ce72683fbe2a
Meziu <meziu210@icloud.com>
2021-01-20 22:15:44 +0100

    clean project folder

--- build/collision.d
+++
@@ -1,5 +0,0 @@
-collision.o: /home/andrea/Desktop/3ds-prism/source/collision.c \
- /home/andrea/Desktop/3ds-prism/include/collision.h \
- /home/andrea/Desktop/3ds-prism/include/vector2D.h
-/home/andrea/Desktop/3ds-prism/include/collision.h:
-/home/andrea/Desktop/3ds-prism/include/vector2D.h:
--- build/vector2D.d
+++
@@ -1,3 +0,0 @@
-vector2D.o: /home/andrea/Desktop/3ds-prism/source/vector2D.c \
- /home/andrea/Desktop/3ds-prism/include/vector2D.h
-/home/andrea/Desktop/3ds-prism/include/vector2D.h:
